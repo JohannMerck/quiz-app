d39e9f5159cfba6945383ff8f74a9559036cc3b5
Johann <johannmerck@gmail.com>
2024-07-10 16:58:52 +0200

    yoyoy

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// phew… not a lot going on here. Please add some code!
+const bodyElement = document.querySelector('[data-js="body"]');
+
+const bookMarkButton = document.querySelector(
+  '[data-js="button-bookmark-empty"]'
+);
+
+bookMarkButton.addEventListener("click", () => {
+  bookMarkButton.classList.toggle("bookmark--active");
+});
+
+const answerButton = document.querySelector('[data-js="answer-button"]');
+const answerHidden = document.querySelector('[data-js="answer-hidden"]');
+
+answerButton.addEventListener("click", () => {
+  answerHidden.classList.toggle("card__answer--active");
+  if (answerButton.textContent === "Hide Answer") {
+    answerButton.textContent = "Show Answer";
+  } else {
+    answerButton.textContent = "Hide Answer";
+  }
+});
